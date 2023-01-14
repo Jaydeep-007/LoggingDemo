f9e468a652de2ce4eebabfc76edfc1c5ec7a10d4
Jaydeep-07 <jaydeepvpatil225@gmail.com>
2023-01-14 10:35:16 +0530

    Add project files.

---
+++ LoggingDemo/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿
+using LoggingDemo.Logging;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoggingDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly Logger logger;
+        public TestController(Logger _logger)
+        {
+            logger = _logger;   
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public string Login()
+        {
+            _ = logger.Log("login succesfull");
+            return "You have logged in successfully";
+        }
+    }
+}
---
+++ LoggingDemo/Startup.cs
@@ -0,0 +1,64 @@
+using LoggingDemo.Logging;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace LoggingDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            var isEnabled = Configuration.GetSection("IsLoggerEnabled").Value;
+            if (isEnabled == "true")
+            {
+                services.AddSingleton<Logger>();
+            }
+
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            // This middleware serves generated Swagger document as a JSON endpoint
+            app.UseSwagger();
+
+            // This middleware serves the Swagger documentation UI
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "LoggingDemo API V1");
+            });
+
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
